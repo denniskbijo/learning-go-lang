60b6ce34b371889ee7ab1e77e10f377805cd5458
denniskbijo <denniskbijo7@gmail.com>
2024-04-15 12:22:59 +0100

    LGv1: string and int variable types

---
+++ practice/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	var aString string ="this is Go"
+	fmt.Println(aString)
+	fmt.Printf("the variable type is %T\n", aString)
+
+	var anInteger int =43
+	fmt.Println(anInteger)
+}
