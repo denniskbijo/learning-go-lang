36a9202cfb7908b36b1ebfe1211f9d3238f3c4c9
denniskbijo <denniskbijo7@gmail.com>
2024-04-25 13:29:26 +0100

    LGv1: Comment lines to clear console

--- practice/main.go
+++ practice/main.go
@@ -1,71 +1,71 @@
 package main
 
 import (
-	"bufio"
+	// "bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
+	// "os"
+	// "strconv"
+	// "strings"
 )
 
-const aConst int = 32
+// const aConst int = 32
 
-const implicitConst = 67
+// const implicitConst = 67
 
 func main() {
 
 	// Practice variables, types and constants printing
 
-	var aString string = "this is Go"
-	fmt.Println(aString)
-	fmt.Printf("the variable type is %T\n", aString)
+	// var aString string = "this is Go"
+	// fmt.Println(aString)
+	// fmt.Printf("the variable type is %T\n", aString)
 
-	var anInteger int = 43
-	fmt.Println(anInteger)
+	// var anInteger int = 43
+	// fmt.Println(anInteger)
 
-	var defaultInt int
-	// Prints 0
-	fmt.Println(defaultInt)
+	// var defaultInt int
+	// // Prints 0
+	// fmt.Println(defaultInt)
 
-	var defaultString string
-	// Prints empty string
-	fmt.Println(defaultString)
+	// var defaultString string
+	// // Prints empty string
+	// fmt.Println(defaultString)
 
-	var implicitInt = 52
-	// Prints 52
-	fmt.Println(implicitInt)
+	// var implicitInt = 52
+	// // Prints 52
+	// fmt.Println(implicitInt)
 
-	var implicitString = "This is implicityl a string"
-	fmt.Println(implicitString)
+	// var implicitString = "This is implicityl a string"
+	// fmt.Println(implicitString)
 
-	// This will only work inside a function
-	myString := "This is string without var"
-	fmt.Println(myString)
+	// // This will only work inside a function
+	// myString := "This is string without var"
+	// fmt.Println(myString)
 
-	fmt.Println(aConst)
-	fmt.Println(implicitConst)
+	// fmt.Println(aConst)
+	// fmt.Println(implicitConst)
 
-	// Practice getting input from console
+	// // Practice getting input from console
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter Text: ")
+	// reader := bufio.NewReader(os.Stdin)
+	// fmt.Print("Enter Text: ")
 
-	//Var _ is a variable we can ignore as it is used to handle error here
-	input, _ := reader.ReadString('\n')
-	fmt.Println("You entered: ", input)
+	// //Var _ is a variable we can ignore as it is used to handle error here
+	// input, _ := reader.ReadString('\n')
+	// fmt.Println("You entered: ", input)
 
-	//Convert input to float type
-	fmt.Print("Enter a number: ")
+	// //Convert input to float type
+	// fmt.Print("Enter a number: ")
 
-	//Var err is an error object used to handle error in conversion here
-	numInput, _ := reader.ReadString('\n')
-	fmt.Println("You entered: ", numInput)
+	// //Var err is an error object used to handle error in conversion here
+	// numInput, _ := reader.ReadString('\n')
+	// fmt.Println("You entered: ", numInput)
 
-	afloat, err := strconv.ParseFloat(strings.TrimSpace(numInput), 64)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("Parsed number is: ", afloat)
-	}
+	// afloat, err := strconv.ParseFloat(strings.TrimSpace(numInput), 64)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println("Parsed number is: ", afloat)
+	// }
 
 }
